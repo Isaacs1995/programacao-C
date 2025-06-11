1365b2f1efd0f4e12acb94f37fbff9189a7e91de
Isaac Oliveira <127420401+Isaacs1995@users.noreply.github.com>
2025-06-11 12:07:08 +0000

    INICIO

--- Jogo de Xadrez.c
+++ Jogo de Xadrez.c
@@ -2,7 +2,36 @@
 
 int main(){
 
+    //TORRE
+int torre = 1;
 
+printf("Movimentação da torre, 5 casas a direita: \n");
+while (torre <= 5)
+{
+    printf("Direita\n");
+    torre++;
+}
+
+    //BISPO
+int bispo = 1;
+
+printf("\nMovimentação diagonal do bispo, 5 casas à cima e à direita: \n");
+
+do
+{
+    printf("Cima,Direita\n");
+    bispo++;
+} while (bispo <= 5);
+
+    //RAINHA
+int rainha;
+
+printf("\nMovimentação da rainha, 8 casas à esquerda: \n");
+
+for (rainha = 1; rainha <= 8; rainha++)
+{
+    printf("Esquerda \n");
+}
 
     return 0;
 }
