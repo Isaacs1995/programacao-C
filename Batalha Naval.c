31a681c0dcabaa52212bd0f6376a4bc755548a7d
Isaac Oliveira <127420401+Isaacs1995@users.noreply.github.com>
2025-06-11 15:34:43 +0000

    Criação da Matriz e posicionamento dos navios

--- Batalha Naval.c
+++ Batalha Naval.c
@@ -1,1 +1,51 @@
+#include <stdio.h>
 
+int main(){
+
+    // Criando a Matriz
+    int matriz[10][10]= {
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0}
+    };
+
+    // Posicionando os navios
+    for (int i = 0; i < 3; i++)
+    {
+        matriz[0][i] = 3;
+    }
+    
+    for (int j = 4; j < 7; j++)
+    {
+        matriz[j][5] = 3;
+    }
+    
+    // Exibindo o tabuleiro
+    printf("Tabuleiro Batalha Naval:\n\n");
+    printf("  "); 
+
+    // Exibindo as letras das colunas
+    for (int j = 0; j < 10; j++) {
+        printf("%c ", 'A' + j);
+    }
+    printf("\n");
+
+
+    for (int i = 0; i < 10; i++) {
+        printf("%2d ", i + 1);// Exibindo o número de cada linha
+        for (int j = 0; j < 10; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
